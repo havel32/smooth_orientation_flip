15a340596e9a24eb4e21a80e18e94c252fa08f0b
ian <havelkaslana@gmail.com>
2025-03-19 13:01:10 +0300

    formatted view of videopage and added butttons to full screen

--- lib/full_screen_page.dart
+++ lib/full_screen_page.dart
@@ -4,14 +4,16 @@ import 'package:flutter/services.dart';
 import 'package:video_player/video_player.dart';
 
 class FullScreenVideo extends StatefulWidget {
-  final VideoPlayerController controller;
+  final VideoPlayerController _controller;
 
-  const FullScreenVideo({super.key, required this.controller});
+  const FullScreenVideo({super.key, required VideoPlayerController controller})
+    : _controller = controller;
   @override
   State<FullScreenVideo> createState() => _FullScreenVideoState();
 }
 
 class _FullScreenVideoState extends State<FullScreenVideo> {
+  bool isVideoRotated = false;
   @override
   void initState() {
     super.initState();
@@ -35,7 +37,8 @@ class _FullScreenVideoState extends State<FullScreenVideo> {
 
   Future<void> buildCompleteActions() async {
     await Future.delayed(Durations.short2);
-    changeOrientation();
+    await changeOrientation();
+    isVideoRotated = true;
   }
 
   @override
@@ -43,37 +46,69 @@ class _FullScreenVideoState extends State<FullScreenVideo> {
     timeDilation = 1;
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Stack(
-        children: [
-          Align(
-            alignment: Alignment.center,
-            child: Hero(
-              tag: "video",
+      body: Hero(
+        tag: 'video',
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.center,
               child: AspectRatio(
-                aspectRatio: widget.controller.value.aspectRatio,
-                child: VideoPlayer(widget.controller),
+                aspectRatio: widget._controller.value.aspectRatio,
+                child: SizedBox(child: VideoPlayer(widget._controller)),
               ),
             ),
-          ),
-          Align(
-            alignment: Alignment.bottomRight,
-            child: IconButton(
-              icon: const Icon(Icons.fullscreen_exit, color: Colors.white),
-              onPressed: () async {
-                await Future.microtask(() async {
-                  await SystemChrome.setPreferredOrientations([
-                    DeviceOrientation.portraitUp,
-                  ]);
-                  await SystemChrome.setEnabledSystemUIMode(
-                    SystemUiMode.edgeToEdge,
-                  );
-                });
-                await Future.delayed(Durations.medium2);
-                if (context.mounted) Navigator.pop(context);
-              },
+            // ),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Padding(
+                padding: const EdgeInsets.only(right: 80),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.black.withAlpha(150),
+                    borderRadius: BorderRadius.circular(18),
+                  ),
+                  child: IconButton(
+                    icon: const Icon(
+                      Icons.fullscreen_exit,
+                      color: Colors.white,
+                    ),
+                    onPressed: () async {
+                      await Future.microtask(() async {
+                        await SystemChrome.setPreferredOrientations([
+                          DeviceOrientation.portraitUp,
+                        ]);
+                        await SystemChrome.setEnabledSystemUIMode(
+                          SystemUiMode.edgeToEdge,
+                        );
+                      });
+                      await Future.delayed(Durations.short1);
+                      if (context.mounted) Navigator.pop(context);
+                    },
+                  ),
+                ),
+              ),
             ),
-          ),
-        ],
+            Align(
+              alignment: Alignment.center,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.black.withAlpha(150),
+                  shape: BoxShape.circle,
+                ),
+                child: IconButton(
+                  onPressed: () {
+                    setState(() {
+                      widget._controller.value.isPlaying
+                          ? widget._controller.pause()
+                          : widget._controller.play();
+                    });
+                  },
+                  icon: Icon(Icons.play_arrow, color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/video_page.dart
+++ lib/video_page.dart
@@ -44,88 +44,90 @@ class _VideoAppState extends State<VideoApp>
     double videplayerWidth = _controller.value.aspectRatio * videplayerHeight;
     return Scaffold(
       appBar: AppBar(title: const Text('Video Demo')),
-      body: SingleChildScrollView(
-        child: ConstrainedBox(
-          constraints: BoxConstraints(
-            maxHeight:
-                MediaQuery.of(context).size.height, // Ограничиваем высоту
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              _controller.value.isInitialized
-                  ? SizedBox(
-                    height: videplayerHeight,
-                    width: videplayerWidth,
-                    child: Stack(
-                      children: [
-                        Align(
-                          alignment: Alignment.center,
-                          child: AspectRatio(
-                            aspectRatio: _controller.value.aspectRatio,
-                            child: Hero(
-                              tag: 'video',
-                              child: SizedBox(
-                                height: videplayerHeight,
-                                width: videplayerWidth,
-                                child: VideoPlayer(_controller),
-                              ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              maxHeight: MediaQuery.of(context).size.height,
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(
+                  height: videplayerHeight,
+                  width: videplayerWidth,
+                  child:
+                      _controller.value.isInitialized
+                          ? Hero(
+                            tag: 'video',
+                            child: Stack(
+                              children: [
+                                Align(
+                                  alignment: Alignment.center,
+                                  child: AspectRatio(
+                                    aspectRatio: _controller.value.aspectRatio,
+                                    child: SizedBox(
+                                      height: videplayerHeight,
+                                      width: videplayerWidth,
+                                      child: VideoPlayer(_controller),
+                                    ),
+                                  ),
+                                ),
+                                // ),
+                                Align(
+                                  alignment: Alignment.bottomRight,
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      color: Colors.black.withAlpha(150),
+
+                                      borderRadius: BorderRadius.circular(18),
+                                    ),
+                                    child: IconButton(
+                                      icon: const Icon(
+                                        Icons.fullscreen,
+                                        color: Colors.white,
+                                      ),
+                                      onPressed: _toggleFullScreen,
+                                    ),
+                                  ),
+                                ),
+                                Align(
+                                  alignment: Alignment.center,
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      color: Colors.black.withAlpha(150),
+                                      shape: BoxShape.circle,
+                                    ),
+                                    child: IconButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          _controller.value.isPlaying
+                                              ? _controller.pause()
+                                              : _controller.play();
+                                        });
+                                      },
+                                      icon: Icon(
+                                        Icons.play_arrow,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
                             ),
-                          ),
-                        ),
-                        Align(
-                          alignment: Alignment.bottomRight,
-                          child: IconButton(
-                            icon: const Icon(
-                              Icons.fullscreen,
-                              color: Colors.black,
-                            ),
-                            onPressed: _toggleFullScreen,
-                          ),
-                        ),
-                        Align(
-                          alignment: Alignment.center,
-                          child: IconButton(
-                            onPressed: () {
-                              setState(() {
-                                _controller.value.isPlaying
-                                    ? _controller.pause()
-                                    : _controller.play();
-                              });
-                            },
-                            icon: Icon(Icons.play_arrow, color: Colors.white),
-                          ),
-                        ),
-                      ],
-                    ),
-                  )
-                  : Container(),
-              Expanded(
-                child: GridView.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    childAspectRatio: 1.6,
-                  ),
-                  itemCount: 18,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      color: Colors.red,
-                      child: Stack(
-                        children: [
-                          Image.network(
-                            fit: BoxFit.fill,
-                            width: double.infinity,
-                            height: double.infinity,
-                            "https://img.freepik.com/free-photo/street-with-trees-sides_1194-1228.jpg?semt=ais_hybrid",
-                          ),
-                          Text("dwadawdawdawdawd"),
-                        ],
-                      ),
-                    );
-                  },
+                          )
+                          : SizedBox.shrink(),
                 ),
-              ),
-            ],
+                const SizedBox(height: 8),
+                InputDecorator(
+                  decoration: InputDecoration(border: OutlineInputBorder()),
+                  child: Text("Camera 1"),
+                ),
+                const SizedBox(height: 8),
+                RecordsGridList(),
+              ],
+            ),
           ),
         ),
       ),
@@ -136,5 +138,38 @@ class _VideoAppState extends State<VideoApp>
   void dispose() {
     _controller.dispose();
     super.dispose();
+  }
+}
+
+class RecordsGridList extends StatelessWidget {
+  const RecordsGridList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 1.6,
+        ),
+        itemCount: 18,
+        itemBuilder: (context, index) {
+          return Container(
+            color: Colors.red,
+            child: Stack(
+              children: [
+                Image.network(
+                  fit: BoxFit.fill,
+                  width: double.infinity,
+                  height: double.infinity,
+                  "https://img.freepik.com/free-photo/street-with-trees-sides_1194-1228.jpg?semt=ais_hybrid",
+                ),
+                Text("dwadawdawdawdawd"),
+              ],
+            ),
+          );
+        },
+      ),
+    );
   }
 }
